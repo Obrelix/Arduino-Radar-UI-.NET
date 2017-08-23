df0d5cbb291bc5f2ca3e28c9fff9dae0a773c71e
Ioannis Alimpertis <ialimpertis@outlook.com>
2017-08-23 23:39:35 +0300

    Arduino Code minor changes

--- ArduinoRadar/ArduinoRadar.ino
+++ ArduinoRadar/ArduinoRadar.ino
@@ -1,45 +1,33 @@
 #include <Servo.h>
-
-const int trigPin = 11, echoPin = 10, servoPin = 2;
-long distance , duration;
-int Angle, turnLeft;
-String outputString;
-Servo myServo; //Creating a servo object
+int trigPin, echoPin, servoPin, Angle, turnLeft;
+Servo radarServo; //Creating a servo object
 
 void setup() 
 {
-  distance = 0; duration = 0; Angle = 0; turnLeft = 1;
+  trigPin = 11; echoPin = 10; servoPin = 2; Angle = 10; turnLeft = 1;
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-  myServo.attach(servoPin); // Defines on which pin is the servo motor attached
-  Serial.begin(9600); //Begin a sirial communication
-  distance = 0; duration = 0;
-  Angle = 10; turnLeft = 1;
+  radarServo.attach(servoPin); // Defines on which pin is the servo motor attached
+  Serial.begin(9600); // Begin a sirial communication
 }
 
 void loop() 
 {
   if (Angle < 170 && turnLeft) Angle ++;
   else if (Angle >= 170 || !turnLeft) { turnLeft = (Angle <= 11); Angle--; }
-  myServo.write(Angle);
-  distance = calculateDistance();
-  outputString = String(Angle) + "," + String(distance) + ".\n";
-  Serial.print(outputString);
+  radarServo.write(Angle);
+  Serial.print(String(Angle) + "," + String(calculateDistance()) + ".\n");
   delay(50);
 }
 
 long calculateDistance()
 { 
-  //clean the trigPin to begin the measurment
-  digitalWrite(trigPin, LOW); 
+  digitalWrite(trigPin, LOW); // Clean the trigPin to begin the measurment
   delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH); 
+  digitalWrite(trigPin, HIGH); // Sets the trigPin on HIGH state for 10 Microseconds
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH); // Reads the echoPin, returns the sound wave travel time in microseconds
-  //U(m/s)=dX(m)/dT(s) 
-  // In dry air at 20 °C, the speed of sound is 343.2 m/s or 0.003432 m/Microsecond or 0,03434 cm/Microseconds
-  return duration*0.034/2;
+  //U(m/s)=dX(m)/dT(s) In dry air at 20 °C, the speed of sound is 343.2 m/s or 0.003432 m/Microsecond or 0,03434 cm/Microseconds
+  return pulseIn(echoPin, HIGH) * 0.034 / 2; // Reads the echoPin, returns the sound wave travel time in microseconds
 }
 
